78280e0765c920c9570376b1a2bcfbbed501afc8
burturt <31748545+burturt@users.noreply.github.com>
2020-06-08 17:49:31 -0700

    Add brute force ability
    Add brute force ability

--- railfence.py
+++ railfence.py
@@ -11,21 +11,22 @@
 def printFence(fence):
     for rail in range(len(fence)):
         print ''.join(fence[rail])
-    
+
+
 def encryptFence(plain, rails, offset=0, debug=False):
     cipher = ''
 
     # offset
-    plain = '#'*offset + plain
+    plain = '#' * offset + plain
 
     length = len(plain)
-    fence = [['#']*length for _ in range(rails)]
+    fence = [['#'] * length for _ in range(rails)]
 
     # build fence
     rail = 0
     for x in range(length):
         fence[rail][x] = plain[x]
-        if rail >= rails-1:
+        if rail >= rails - 1:
             dr = -1
         elif rail <= 0:
             dr = 1
@@ -48,26 +49,26 @@ def decryptFence(cipher, rails, offset=0, debug=False):
 
     # offset
     if offset:
-        t = encryptFence('o'*offset + 'x'*len(cipher), rails)
+        t = encryptFence('o' * offset + 'x' * len(cipher), rails)
         for i in range(len(t)):
-            if(t[i] == 'o'):
+            if (t[i] == 'o'):
                 cipher = cipher[:i] + '#' + cipher[i:]
-    
+
     length = len(cipher)
-    fence = [['#']*length for _ in range(rails)]
+    fence = [['#'] * length for _ in range(rails)]
 
     # build fence
     i = 0
     for rail in range(rails):
-        p = (rail != (rails-1))
+        p = (rail != (rails - 1))
         x = rail
         while (x < length and i < length):
             fence[rail][x] = cipher[i]
             if p:
-                x += 2*(rails - rail - 1)
+                x += 2 * (rails - rail - 1)
             else:
-                x += 2*rail
-            if (rail != 0) and (rail != (rails-1)):
+                x += 2 * rail
+            if (rail != 0) and (rail != (rails - 1)):
                 p = not p
             i += 1
 
@@ -84,9 +85,26 @@ def decryptFence(cipher, rails, offset=0, debug=False):
 
 
 if __name__ == "__main__":
-    plain = "Proprietary software tends to have malicious features. The point is with a proprietary program, when the users don't have the source code, we can never tell. So you must consider every proprietary program as potential malware."
-    print plain
-    cipher = encryptFence(plain, 6, offset=4, debug=True)
-    print cipher
-    plain2 = decryptFence(cipher, 6, offset=4, debug=True)
-    print plain2
+
+    import sys
+
+    # CHANGE THIS
+    # THE LAST NUMBER DOES NOT GET USED
+    rails = range(2, 11)
+    offsets = range(0, 11)
+
+    # DONT CHANGE THIS
+
+    if len(sys.argv) != 2:
+        print("Usage: python " + sys.argv[0] + " [ciphertext]")
+        print("You may need to use quotes around the ciphertext if it contains spaces")
+        exit(1)
+    ciphertext = sys.argv[1]
+    print("-----------------------------")
+    print "Brute forcing all rails and offsets:"
+
+    print(rails)
+    print(offsets)
+    for i in rails:
+        for j in offsets:
+            print decryptFence(ciphertext, i, offset=j, debug=False)
